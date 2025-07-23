3c2adb690eee507450d9ff9abab0e823b5ca1cfe
Savio Babu <137677016+saviobabu2@users.noreply.github.com>
2025-07-23 18:47:01 +0530

    3rd commit

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+  console.log(`🚀 Server is running at http://localhost:${PORT}/admin/`);
 });
 
 
